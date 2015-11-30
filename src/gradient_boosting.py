c7ce13add5661fdde9da869a129cef7dedec9713
wilsonli5000 <wilsonli5000@gmail.com>
2015-11-29 22:31:42 -0800

    parameter tuned by wilson li

--- src/gradient_boosting.py
+++ src/gradient_boosting.py
@@ -9,15 +9,15 @@ train_x, train_y = prepare.get_exclude_ndf_x(train_data, basic_users_info, label
 validation_x, validation_y = prepare.get_exclude_ndf_x(validation_data, basic_users_info, label_encoder)
 
 
-for i in range(1, 50):
-	rf = OneVsRestClassifier(GradientBoostingClassifier(learning_rate=0.05, n_estimators=i)).fit(train_x, train_y)
-	validation_predict_proba = rf.predict_proba(validation_x)
-	class_order = rf.classes_
-	predict_list = evaluate.candidate_classes(validation_predict_proba, class_order)
-	ndcg = evaluate.ndcg(predict_list, validation_data)
-#print ndcg
+#for i in range(1, 50):
+rf = OneVsRestClassifier(GradientBoostingClassifier(learning_rate=0.1, n_estimators=11)).fit(train_x, train_y)
+validation_predict_proba = rf.predict_proba(validation_x)
+class_order = rf.classes_
+predict_list = evaluate.candidate_classes(validation_predict_proba, class_order)
+ndcg = evaluate.ndcg(predict_list, validation_data)
+print ndcg
 	#ndcg_list.append(ndcg)
-	print i, " ", ndcg
+	#print i, " ", ndcg
 
 
 test_x = prepare.get_exclude_ndf_test_x(test_data, basic_users_info, label_encoder)
